218bd39740d579aac198487303c8609bc2b767a4
samueldelagrange30-maker <samueldelagrange30-maker@users.noreply.github.com>
2026-09-15 06:38:32 +0000

    Fix Catalogue deep links on GitHub Pages
    Switch to HashRouter and ship 404.html so catalogue URLs work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { CollectionProvider } from './hooks/useCollection';
 import { Catalogue } from './pages/Catalogue';
@@ -10,7 +10,7 @@ import { Packs } from './pages/Packs';
 export default function App() {
   return (
     <CollectionProvider>
-      <BrowserRouter basename="/tcg-packs-combat">
+      <HashRouter>
         <Routes>
           <Route element={<Layout />}>
             <Route index element={<Home />} />
@@ -21,7 +21,7 @@ export default function App() {
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </CollectionProvider>
   );
 }
